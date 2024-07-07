dfdc877defe59d477ce56701cde969671ccd3b55
JungHyunSeok <140382912+Junghs21@users.noreply.github.com>
2024-07-07 21:18:18 +0900

    [Bronze V] Title: A+B - 4, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int A, B;
+
+	while (true)
+	{
+		cin >> A >> B;
+
+		if (cin.eof()) {
+			break;
+		}
+		else {
+			cout << A + B << "\n";
+		}
+	}
+}
