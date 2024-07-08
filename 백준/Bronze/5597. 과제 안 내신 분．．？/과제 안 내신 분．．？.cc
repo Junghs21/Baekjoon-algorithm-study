0dcb10a12140b293f89b0ec26af4cc16197149ca
JungHyunSeok <140382912+Junghs21@users.noreply.github.com>
2024-07-08 15:46:22 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
@@ -1,36 +1,5 @@
-//#include <iostream>
-//#include <vector>
-//#include <algorithm>
-//
-//using namespace std;
-//
-//int main() {
-//
-//	vector<int> attendStudent(28);
-//	vector<int> absenceStudent;
-//
-//	for (int i = 0; i < 28;i++) {
-//		cin >> attendStudent[i];
-//	}
-//
-//	sort(attendStudent.begin(), attendStudent.end());
-//
-//	for (int j = 0; j < attendStudent.size() - 1;j++) {
-//		if (attendStudent[j] != attendStudent[j + 1] - 1) {
-//			absenceStudent.push_back(attendStudent[j + 1] - 1);
-//		}
-//	}
-//
-//	sort(absenceStudent.begin(), absenceStudent.end());
-//
-//	for (int i = 0; i < absenceStudent.size(); i++) {
-//		cout << absenceStudent[i] << "\n";
-//	}
-//}
-
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
