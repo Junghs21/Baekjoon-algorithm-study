1cff8d36bff60aa95e0bbae22c2284b9ae86008d
JungHyunSeok <140382912+Junghs21@users.noreply.github.com>
2024-08-07 01:58:12 +0900

    [Silver V] Title: 숫자 카드, Time: 224 ms, Memory: 6072 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int numberOfsanggeunCard;
+	cin >> numberOfsanggeunCard;
+
+	vector<int> sanggeunCard(numberOfsanggeunCard);
+
+	for (int i = 0; i < numberOfsanggeunCard; i++) {
+		cin >> sanggeunCard[i];
+	}
+
+	sort(sanggeunCard.begin(), sanggeunCard.end());
+
+	int numberOfCard;
+	cin >> numberOfCard;
+
+	vector<int> CardNums(numberOfCard);
+
+	for (int i = 0; i < numberOfCard;i++) {
+		cin >> CardNums[i];
+	}
+
+	vector<bool> result(numberOfCard, false);
+
+
+	for (int i = 0; i < numberOfCard;i++) {
+
+		int left = 0;
+		int right = sanggeunCard.size() - 1;		
+
+		while ((left <= right) && !(result[i])) {
+			int mid = left + (right - left) / 2;
+
+			if (CardNums[i] == sanggeunCard[mid]) {
+				result[i] = true;
+			}
+			else if (sanggeunCard[mid] < CardNums[i]) {
+				left = mid + 1;
+			}
+			else {
+				right = mid - 1;
+			}
+		}
+	}
+
+	for (int i = 0; i < result.size(); i++) {
+		cout << result[i] << " ";
+	}
+}
