2cd44267f630c0c1aa34a57b423c473aebba5530
JungHyunSeok <140382912+Junghs21@users.noreply.github.com>
2024-08-22 01:39:44 +0900

    [Silver II] Title: 연속합, Time: 20 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -9,24 +9,20 @@ int main() {
 	int N;
 	cin >> N;
 
-	int result = -1000;
-
-	vector<int> nums(N, 0);
-	for (int i = 0; i < N; i++) {
+	vector<int> nums(N);
+	for (int i = 0; i < N;i++) {
 		cin >> nums[i];
 	}
 
-	vector<int> dp(N, 0);
+	vector<int> dp(N);
+
 	dp[0] = nums[0];
-	for (int i = 1; i < N; i++) {
+	int result = dp[0];
+
+	for (int i = 1; i < nums.size(); i++) {
 		dp[i] = max(nums[i], dp[i - 1] + nums[i]);
 		result = max(result, dp[i]);
 	}
 
-	if (result < dp[0]) {
-		result = dp[0];
-	}
-
 	cout << result;
-
 }
